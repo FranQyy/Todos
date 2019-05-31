a85586cb929d7de106368f56584af1e0a04e8a86
FranQyy <lfczerniak@gmail.com>
2019-05-31 23:32:30 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"></Todos>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos'
+import Header from './components/layout/Header'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Todos,
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "To do 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "To do 2",
+          completed: false
+        },
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+      <h2>Todo list</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+}
+</script>
+
+<style>
+  .header{
+    background: black;
+    color: white;
+    padding: 15px;
+  }
+</style>
